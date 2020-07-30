2bb0122077ff300207f8e3b87b4ef836bce8fc19
TechnologicalPizza <piatkowski2002@gmail.com>
2020-07-30 02:39:36 +0200

    Improved VectorType

--- MonoGame.Utilities/Vectors/VectorType.cs
+++ MonoGame.Utilities/Vectors/VectorType.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace MonoGame.Framework.Vectors
 {
-    public class VectorType
+    // Using a sealed generic class and overriding abstract members
+    // allows the JIT to perform special optimizations.
+
+    public abstract class VectorType
     {
-        private static ConcurrentDictionary<Type, VectorType> GetCache { get; } =
+        private static ConcurrentDictionary<Type, VectorType> Cache { get; } =
             new ConcurrentDictionary<Type, VectorType>();
 
-        public Type Type { get; }
-        public int ElementSize { get; }
         public VectorComponentInfo ComponentInfo { get; }
+
+        public abstract Type Type { get; }
+        public abstract int ElementSize { get; }
 
         public int BitDepth => ComponentInfo.BitDepth;
 
-        public VectorType(Type type, VectorComponentInfo componentInfo)
+        public VectorType(VectorComponentInfo componentInfo)
         {
-            Type = type;
-            ElementSize = Marshal.SizeOf(type);
             ComponentInfo = componentInfo;
         }
 
@@ -30,7 +30,7 @@ namespace MonoGame.Framework.Vectors
             // Using a static generic class should allow the JIT to devirtualize the getter,
             // granting performance equal to accessing a static field 
             // instead of a (expensive in comparison) dictionary lookup when using a Type.
-            return Helper<TVector>.Instance;
+            return VectorType<TVector>.Instance;
         }
 
         public static VectorType Get(Type vectorType)
@@ -38,18 +38,28 @@ namespace MonoGame.Framework.Vectors
             static VectorType ValueFactory(Type type)
             {
                 // MakeGenericType validates generic constraints.
-                var helper = typeof(Helper<>).MakeGenericType(type);
-                var property = helper.GetProperties()[0];
+                var helper = typeof(VectorType<>).MakeGenericType(type);
+                
+                var property = helper.GetProperty("Instance");
+                if (property == null)
+                    throw new Exception("Could not load required property for reflection.");
+
                 return (VectorType)property.GetValue(null)!;
             }
-            return GetCache.GetOrAdd(vectorType, ValueFactory);
+            return Cache.GetOrAdd(vectorType, ValueFactory);
         }
+    }
 
-        private static class Helper<TVector>
-            where TVector : unmanaged, IVector
+    internal sealed class VectorType<TVector> : VectorType
+        where TVector : unmanaged, IVector
+    {
+        public static VectorType<TVector> Instance { get; } = new VectorType<TVector>();
+
+        public override Type Type => typeof(TVector);
+        public override int ElementSize => Unsafe.SizeOf<TVector>();
+
+        public VectorType() : base(new TVector().ComponentInfo)
         {
-            public static VectorType Instance { get; } =
-                new VectorType(typeof(TVector), new TVector().ComponentInfo);
         }
     }
 }
