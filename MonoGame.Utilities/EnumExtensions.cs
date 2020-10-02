0d4564a0ff182e5c8c74570984a9d4b4492ebd86
TechnologicalPizza <piatkowski2002@gmail.com>
2020-10-02 23:37:20 +0200

    Added HasAnyFlag to EnumExtensions

--- MonoGame.Utilities/EnumExtensions.cs
+++ MonoGame.Utilities/EnumExtensions.cs
@@ -5,107 +5,230 @@ namespace MonoGame.Framework
 {
     public static class EnumExtensions
     {
+        #region HasFlags
+
         /// <summary>
-        /// Determines whether a value has the specified flags.
+        /// Determines whether the value is equal to the given mask.
         /// </summary>
         /// <typeparam name="TEnum">The type of the enum.</typeparam>
-        /// <param name="value">The value.</param>
-        /// <param name="flags">The flag.</param>
-        /// <returns>
-        ///  <see langword="true"/> if the specified value has flags; otherwise, <see langword="false"/>.
-        /// </returns>
-        public static bool HasFlags<TEnum>(this TEnum value, TEnum flags) where TEnum : Enum
+        public static bool HasFlags<TEnum>(this TEnum value, TEnum mask) where TEnum : Enum
         {
-            return EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flags);
+            return HasFlagsHelper<TEnum>.Func(value, mask);
         }
 
+        /// <summary>
+        /// Determines whether the value is equal to any of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
         public static bool HasFlags<TEnum>(
-            this TEnum value, TEnum flag0, TEnum flag1) where TEnum : Enum
+            this TEnum value, TEnum mask0, TEnum mask1) where TEnum : Enum
         {
-            if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag0) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag1))
-                return true;
-            return false;
-        }
-
-        public static bool HasFlags<TEnum>(
-            this TEnum value, TEnum flag0, TEnum flag1, TEnum flag2) where TEnum : Enum
-        {
-            if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag0) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag1) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag2))
-                return true;
-            return false;
-        }
-
-        public static bool HasFlags<TEnum>(
-            this TEnum value, TEnum flag0, TEnum flag1, TEnum flag2, TEnum flag3) where TEnum : Enum
-        {
-            if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag0) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag1) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag2) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag3))
-                return true;
-            return false;
-        }
-
-        public static bool HasFlags<TEnum>(
-            this TEnum value, TEnum flag0, TEnum flag1, TEnum flag2, TEnum flag3, TEnum flag4) where TEnum : Enum
-        {
-            if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag0) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag1) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag2) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag3) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag4))
-                return true;
-            return false;
-        }
-
-        public static bool HasFlags<TEnum>(
-            this TEnum value, TEnum flag0, TEnum flag1, TEnum flag2, TEnum flag3, TEnum flag4, TEnum flag5) where TEnum : Enum
-        {
-            if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag0) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag1) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag2) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag3) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag4) ||
-                EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flag5))
+            if (HasFlagsHelper<TEnum>.Func(value, mask0) ||
+                HasFlagsHelper<TEnum>.Func(value, mask1))
                 return true;
             return false;
         }
 
         /// <summary>
-        /// Determines whether a value has any of the given flags.
+        /// Determines whether the value is equal to any of the given masks.
         /// </summary>
         /// <typeparam name="TEnum">The type of the enum.</typeparam>
-        /// <param name="value"></param>
-        /// <param name="flags"></param>
-        /// <returns><see langword="true"/> if the specified value has any flag; otherwise, <see langword="false"/>.</returns>
-        public static bool HasFlags<TEnum>(this TEnum value, ReadOnlySpan<TEnum> flags) where TEnum : Enum
+        public static bool HasFlags<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2) where TEnum : Enum
         {
-            for (int i = 0; i < flags.Length; i++)
-                if (EnumExtensionsInternal<TEnum>.HasFlagsDelegate(value, flags[i]))
+            if (HasFlagsHelper<TEnum>.Func(value, mask0) ||
+                HasFlagsHelper<TEnum>.Func(value, mask1) ||
+                HasFlagsHelper<TEnum>.Func(value, mask2))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value is equal to any of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasFlags<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3) where TEnum : Enum
+        {
+            if (HasFlagsHelper<TEnum>.Func(value, mask0) ||
+                HasFlagsHelper<TEnum>.Func(value, mask1) ||
+                HasFlagsHelper<TEnum>.Func(value, mask2) ||
+                HasFlagsHelper<TEnum>.Func(value, mask3))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value is equal to any of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasFlags<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3, TEnum mask4) where TEnum : Enum
+        {
+            if (HasFlagsHelper<TEnum>.Func(value, mask0) ||
+                HasFlagsHelper<TEnum>.Func(value, mask1) ||
+                HasFlagsHelper<TEnum>.Func(value, mask2) ||
+                HasFlagsHelper<TEnum>.Func(value, mask3) ||
+                HasFlagsHelper<TEnum>.Func(value, mask4))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value is equal to any of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasFlags<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3, TEnum mask4, TEnum mask5) where TEnum : Enum
+        {
+            if (HasFlagsHelper<TEnum>.Func(value, mask0) ||
+                HasFlagsHelper<TEnum>.Func(value, mask1) ||
+                HasFlagsHelper<TEnum>.Func(value, mask2) ||
+                HasFlagsHelper<TEnum>.Func(value, mask3) ||
+                HasFlagsHelper<TEnum>.Func(value, mask4) ||
+                HasFlagsHelper<TEnum>.Func(value, mask5))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value is equal to any of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasFlags<TEnum>(this TEnum value, ReadOnlySpan<TEnum> masks) where TEnum : Enum
+        {
+            for (int i = 0; i < masks.Length; i++)
+                if (HasFlagsHelper<TEnum>.Func(value, masks[i]))
                     return true;
             return false;
         }
 
         /// <summary>
-        /// Determines whether a value has any of the given flags.
+        /// Determines whether the value is equal to any of the given masks.
         /// </summary>
         /// <typeparam name="TEnum">The type of the enum.</typeparam>
-        /// <param name="value"></param>
-        /// <param name="flags"></param>
-        /// <returns><see langword="true"/> if the specified value has any flag; otherwise, <see langword="false"/>.</returns>
-        public static bool HasFlags<TEnum>(this TEnum value, params TEnum[] flags) where TEnum : Enum
+        public static bool HasFlags<TEnum>(this TEnum value, params TEnum[] masks) where TEnum : Enum
         {
-            return HasFlags(value, flags);
+            return HasFlags(value, masks);
         }
 
-        private static class EnumExtensionsInternal<TEnum> where TEnum : Enum
-        {
-            public static readonly Func<TEnum, TEnum, bool> HasFlagsDelegate = CreateHasFlagDelegate();
+        #endregion
 
-            private static Func<TEnum, TEnum, bool> CreateHasFlagDelegate()
+        #region HasAnyFlag
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given mask.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(this TEnum value, TEnum mask) where TEnum : Enum
+        {
+            return HasAnyFlagHelper<TEnum>.Func(value, mask);
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1) where TEnum : Enum
+        {
+            if (HasAnyFlagHelper<TEnum>.Func(value, mask0) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask1))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2) where TEnum : Enum
+        {
+            if (HasAnyFlagHelper<TEnum>.Func(value, mask0) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask1) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask2))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3) where TEnum : Enum
+        {
+            if (HasAnyFlagHelper<TEnum>.Func(value, mask0) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask1) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask2) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask3))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3, TEnum mask4) where TEnum : Enum
+        {
+            if (HasAnyFlagHelper<TEnum>.Func(value, mask0) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask1) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask2) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask3) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask4))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(
+            this TEnum value, TEnum mask0, TEnum mask1, TEnum mask2, TEnum mask3, TEnum mask4, TEnum mask5) where TEnum : Enum
+        {
+            if (HasAnyFlagHelper<TEnum>.Func(value, mask0) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask1) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask2) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask3) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask4) ||
+                HasAnyFlagHelper<TEnum>.Func(value, mask5))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(this TEnum value, ReadOnlySpan<TEnum> masks) where TEnum : Enum
+        {
+            for (int i = 0; i < masks.Length; i++)
+                if (HasAnyFlagHelper<TEnum>.Func(value, masks[i]))
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the value has any flag of the given masks.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        public static bool HasAnyFlag<TEnum>(this TEnum value, params TEnum[] masks) where TEnum : Enum
+        {
+            return HasFlags(value, masks);
+        }
+
+
+        #endregion
+
+        public static class HasFlagsHelper<TEnum> where TEnum : Enum
+        {
+            public static Func<TEnum, TEnum, bool> Func { get; } = CreateFunc();
+
+            private static Func<TEnum, TEnum, bool> CreateFunc()
             {
                 ParameterExpression valueExpression = Expression.Parameter(typeof(TEnum));
                 ParameterExpression flagExpression = Expression.Parameter(typeof(TEnum));
@@ -127,6 +250,29 @@ namespace MonoGame.Framework
                             flagValueVariable),
                         flagValueVariable)
                 );
+
+                var lambda = Expression.Lambda<Func<TEnum, TEnum, bool>>(
+                    body, valueExpression, flagExpression);
+
+                return lambda.Compile();
+            }
+        }
+
+        public static class HasAnyFlagHelper<TEnum> where TEnum : Enum
+        {
+            public static Func<TEnum, TEnum, bool> Func { get; } = CreateFunc();
+
+            private static Func<TEnum, TEnum, bool> CreateFunc()
+            {
+                ParameterExpression valueExpression = Expression.Parameter(typeof(TEnum));
+                ParameterExpression flagExpression = Expression.Parameter(typeof(TEnum));
+                Type flagValueType = Type.GetTypeCode(typeof(TEnum)) == TypeCode.UInt64 ? typeof(ulong) : typeof(long);
+
+                var body = Expression.NotEqual(
+                    Expression.And(
+                        Expression.Convert(valueExpression, flagValueType),
+                        Expression.Convert(flagExpression, flagValueType)),
+                    Expression.Default(flagValueType));
 
                 var lambda = Expression.Lambda<Func<TEnum, TEnum, bool>>(
                     body, valueExpression, flagExpression);
