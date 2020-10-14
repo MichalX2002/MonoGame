d5c22f3d0a1b272e2afd4796d0c2889c67be1237
TechnologicalPizza <piatkowski2002@gmail.com>
2020-10-14 22:04:53 +0200

    Added Array- and ListEnumerator

---
+++ MonoGame.Utilities/Collections/ArrayEnumeratorExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MonoGame.Framework.Collections
+{
+    public static class ArrayEnumeratorExtensions
+    {
+        public static ArrayEnumerator<T> GetArrayEnumerator<T>(this T[] array)
+        {
+            return new ArrayEnumerator<T>(array);
+        }
+    }
+}
